3cfb07877c139dfc76b86ac861ee4d4e04d4a152
Harish Deivanayagam <35465026+HarishDeivanayagam@users.noreply.github.com>
2025-01-20 20:37:58 +0530

    fix: weaviate local url

--- src/lib/weaviate.ts
+++ src/lib/weaviate.ts
@@ -1,11 +1,15 @@
 import weaviate, { WeaviateClient } from "weaviate-client"
 
 export async function getWeaviateClient(): Promise<WeaviateClient> {
-    const url = process.env.WEAVIATE_URL
-    const apiKey = process.env.WEAVIATE_API_KEY
+    const url = process.env.WEAVIATE_URL || ""
+    const apiKey = process.env.WEAVIATE_API_KEY || ""
+    const localUrl = process.env.WEAVIATE_LOCAL_URL || ""
 
-    if (!url || !apiKey) {
-        return await weaviate.connectToLocal()
+    if (localUrl) {
+        return await weaviate.connectToLocal({
+            host: localUrl.split(":")[0],
+            port: parseInt(localUrl.split(":")[1])
+        })
     }
 
     return await weaviate.connectToWeaviateCloud(url, {
